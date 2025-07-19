5cdb6f7f63cc40c5447dcad164975d386b2c149b
Jonathon Robel <jonrrobel@gmail.com>
2025-07-18 21:01:16 -0600

    Much more performant rendering

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const FPS_TARGET = 30;
+const FPS_TARGET = 60;
 const FPS_INTERVAL = 1000 / FPS_TARGET;
+
+const TILE_DIMENSION_IN_PIXELS = 32;
+const GROUND_Y_TARGET = 10;
 
 // Get the canvas element and its 2D rendering context
 const canvas = document.getElementById('backgroundCanvas');
@@ -7,48 +10,17 @@ const ctx = canvas.getContext('2d');
 
 let animationFrameId; // To store the requestAnimationFrame ID for cleanup
 let previousRenderTime;
-let totalRenderedTilesWidth = 0;
-let groundY = 32;
-let scrollOffset = 0;
-const spritesToRender = [];
+
+let leavingBuffer = null;
+let enteringBuffer = null;
+let groundY = GROUND_Y_TARGET;
+let scrollOffset = -50;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function generateNextTileColumn() {
-    if ((totalRenderedTilesWidth * 32) - scrollOffset < canvas.width) {
-        const changeGroundHeight = Math.random() < 0.25;
-        if (changeGroundHeight) {
-            const stepUp = Math.random() < 0.5;
-            if (stepUp) {
-                groundY--;
-            } else {
-                groundY++;
-            }
-        }
-
-        const grass_1 = document.getElementById('sprite_grass_1');
-        const grass_2 = document.getElementById('sprite_grass_2');
-        const grass_3 = document.getElementById('sprite_grass_3');
-        const grass_4 = document.getElementById('sprite_grass_4');
-        const grassSprites = [grass_1, grass_2, grass_3, grass_4];
-        const grassToDraw = grassSprites[getRandomInt(4)];
-        spritesToRender.push({ image: grassToDraw, x: totalRenderedTilesWidth, y: groundY });
-
-        const dirt_1 = document.getElementById('sprite_dirt_1');
-        const dirt_2 = document.getElementById('sprite_dirt_2');
-        const dirt_3 = document.getElementById('sprite_dirt_3');
-        const dirt_4 = document.getElementById('sprite_dirt_4');
-        const dirtSprites = [dirt_1, dirt_2, dirt_3, dirt_4];
-        const dirtToDraw = dirtSprites[getRandomInt(4)];
-        spritesToRender.push({ image: dirtToDraw, x: totalRenderedTilesWidth, y: groundY + 1 });
-        totalRenderedTilesWidth++;
-    }
-}
-
 function drawCanvasContent() {
-    // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     // Create a linear gradient for a subtle background effect
@@ -60,9 +32,8 @@ function drawCanvasContent() {
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
-    for (const sprite of spritesToRender) {
-        ctx.drawImage(sprite.image, (sprite.x * 32) - scrollOffset, sprite.y * 32);
-    }
+    ctx.drawImage(leavingBuffer, scrollOffset, 0);
+    ctx.drawImage(enteringBuffer, scrollOffset + leavingBuffer.width, 0);
 }
 
 function animate() {
@@ -71,11 +42,70 @@ function animate() {
     
     if (elapsedRenderTime > FPS_INTERVAL) {
         previousRenderTime = currentRenderTime - (elapsedRenderTime % FPS_INTERVAL);
-        generateNextTileColumn();
         drawCanvasContent();
-        scrollOffset++;
+        scrollOffset--;
+
+        if (scrollOffset + enteringBuffer.width < 0) {
+            generateLandscapeBuffers();
+            scrollOffset = 0;
+        }
     }
     animationFrameId = requestAnimationFrame(animate);
+}
+
+function generateLandscapeBuffers() {
+    if (leavingBuffer !== null) {
+        generateLandscapeBuffer(leavingBuffer);
+        const newEnteringBuffer = leavingBuffer;
+        leavingBuffer = enteringBuffer;
+        enteringBuffer = newEnteringBuffer;
+        return;
+    }
+    generateLandscapeBuffer(canvas.landscapeBuffer1);
+    generateLandscapeBuffer(canvas.landscapeBuffer2);
+    leavingBuffer = canvas.landscapeBuffer1;
+    enteringBuffer = canvas.landscapeBuffer2;
+}
+
+function generateLandscapeBuffer(buffer) {
+    const landscapeBufferRenderQueue = [];
+    const tileColumnsToGenerate = Math.ceil(canvas.width) / TILE_DIMENSION_IN_PIXELS;
+    
+    const grass_1 = document.getElementById('sprite_grass_1');
+    const grass_2 = document.getElementById('sprite_grass_2');
+    const grass_3 = document.getElementById('sprite_grass_3');
+    const grass_4 = document.getElementById('sprite_grass_4');
+    const grassSprites = [grass_1, grass_2, grass_3, grass_4];
+    
+    const dirt_1 = document.getElementById('sprite_dirt_1');
+    const dirt_2 = document.getElementById('sprite_dirt_2');
+    const dirt_3 = document.getElementById('sprite_dirt_3');
+    const dirt_4 = document.getElementById('sprite_dirt_4');
+    const dirtSprites = [dirt_1, dirt_2, dirt_3, dirt_4];
+    
+    for (let i = 0; i < tileColumnsToGenerate; i++) {
+        const grassToDraw = grassSprites[getRandomInt(4)];
+        landscapeBufferRenderQueue.push({ image: grassToDraw, x: i, y: groundY });
+        for (let j = 1; j <= 20; j++) {
+            const dirtToDraw = dirtSprites[getRandomInt(4)];
+            landscapeBufferRenderQueue.push({ image: dirtToDraw, x: i, y: groundY + j });
+        }
+        const changeGroundHeight = Math.random() < 0.25;
+        if (changeGroundHeight) {
+            const stepUp = Math.random() < (0.5 + ((groundY - GROUND_Y_TARGET) / 50));
+            if (stepUp) {
+                groundY--;
+            } else {
+                groundY++;
+            }
+        }
+    }
+    
+    const bufferContext = buffer.getContext('2d');
+    bufferContext.clearRect(0, 0, canvas.width, canvas.height);
+    for (const sprite of landscapeBufferRenderQueue) {
+        bufferContext.drawImage(sprite.image, (sprite.x * 32), (sprite.y * 32) + (canvas.height / 2));
+    }
 }
 
 function resizeCanvas() {
@@ -85,18 +115,13 @@ function resizeCanvas() {
 }
 
 window.onload = function() {
-    resizeCanvas();
-    
-    const grass_1 = document.getElementById('sprite_grass_1');
-    const grass_2 = document.getElementById('sprite_grass_2');
-    const grass_3 = document.getElementById('sprite_grass_3');
-    const grass_4 = document.getElementById('sprite_grass_4');
-    const grassSprites = [grass_1, grass_2, grass_3, grass_4];
-    for (let i = 0; i < 20; i++) {
-        const grassToDraw = grassSprites[getRandomInt(4)];
-        spritesToRender.push({ image: grassToDraw, x: i, y: 32 });
-    }
-    totalRenderedTilesWidth += 20;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // TODO: These canvases should snap width to next multiple of 32 so they don't overlap
+    canvas.landscapeBuffer1 = new OffscreenCanvas(canvas.width, canvas.height);
+    canvas.landscapeBuffer2 = new OffscreenCanvas(canvas.width, canvas.height);
+    generateLandscapeBuffers();
     
     previousRenderTime = Date.now();
     animate();
